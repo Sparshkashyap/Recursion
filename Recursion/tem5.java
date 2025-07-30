5f46e8ad21096b50a40655d5fe6012e1a4067888
sparshkashyap <sparshkashyap655@gmail.com>
2025-07-31 02:25:29 +0530

    Recursion

--- Recursion/tem5.java
+++ Recursion/tem5.java
@@ -5,8 +5,33 @@ import java.util.Stack;
 
 public class tem5 {
 
+    static void MiddleElement(Stack<Integer> s,int n){
+
+        if(n==1){
+            s.pop();
+            return;
+        }
+
+        int temp = s.pop();
+        MiddleElement(s, n-1);
+        s.push(temp);
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
+        s.push(11);
+        s.push(12);
+        s.push(13);
+        s.push(14);
+        s.push(15);
+
+        int n = s.size()/2 + 1 ;
+
+        MiddleElement(s,n);
+
+        while(!s.isEmpty()){
+            System.out.print(s.pop()+" ");
+        }
         
     }
 
